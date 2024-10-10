bfe5e2f6dda21b9588cfca3a11acee42e56d647f
alfazachmmosi@TimeMachine <zalfa3890@gmail.com>
2024-10-10 15:59:08 +0300

    Adds modified index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = function(a,b) {
+    return a/b;
+};
+const result = divide(2000, 100);
+console.log(result);
+
+
+let x = 2;
+const square = (number) => number * number;
+const calc = square(2);
+console.log(calc);
+
+let a = 3
+let b = 4
+const add = (a,b) => a + b;
+add(3,4);
